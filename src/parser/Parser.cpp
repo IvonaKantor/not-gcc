a55bf0721bab65185999beedba33e0f91ff3964f
ivona <ik87056@student.uwb.edu.pl>
2026-09-06 20:13:40 +0200

    added parser container

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,13 +1,17 @@
 #include "Parser.h"
 #include <vector>
 #include <memory>
+#include <unordered_map>
 using namespace std;
 
 class Parser {
-	private:
-		vector<Token> tokens;
-	     size_t position;
-		unordered_map<string, TokenType> varTable;
+private:
+    vector<Token> tokens;
+    size_t position;
+    unordered_map<string, TokenType> varTable;
 
-
+public:
+    explicit Parser(vector<Token> &tokens)
+        : tokens(tokens), position(0) {
+    }
 };
