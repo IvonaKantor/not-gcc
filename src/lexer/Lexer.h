043971aa5b45662589a54b183e0090e89a946dd7
ivona <ik87056@student.uwb.edu.pl>
2026-09-13 18:33:39 +0200

    added necessary libraries

--- src/lexer/Lexer.h
+++ src/lexer/Lexer.h
@@ -1,8 +1,9 @@
-//
-// Created by iwona on 7/26/2026.
-//
-
 #ifndef NOT_GCC_LEXER_H
 #define NOT_GCC_LEXER_H
 
-#endif //NOT_GCC_LEXER_H
+#include <string>
+#include <vector>
+#include <optional>
+
+
+#endif
