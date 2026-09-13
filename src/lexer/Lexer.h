79152df1c70e7450f8467d6702d44c6605687f11
ivona <ik87056@student.uwb.edu.pl>
2026-09-13 18:47:21 +0200

    explicit lexer

--- src/lexer/Lexer.h
+++ src/lexer/Lexer.h
@@ -1,6 +1,7 @@
 #ifndef NOT_GCC_LEXER_H
 #define NOT_GCC_LEXER_H
 
+#include "Token.h"
 #include <string>
 #include <vector>
 #include <optional>
@@ -11,6 +12,12 @@ private:
     string source;
     size_t position;
     int line;
+
+public:
+    explicit Lexer(const string &source);
+
+    Token nextToken();
+
 };
 
 
