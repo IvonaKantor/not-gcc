03a8748748d7fd5265daba1470a90211bb12e672
ivona <ik87056@student.uwb.edu.pl>
2026-09-18 23:39:30 +0200

    implement token type struct

--- src/Token.h
+++ src/Token.h
@@ -4,13 +4,18 @@
 typedef enum {
     TOKEN_RETURN,
     TOKEN_INT,
+    TOKEN_SEMICOLON,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
+    TOKEN_LBRACE,
+    TOKEN_RBRACE,
     TOKEN_EOF,
     TOKEN_ERROR
 } TokenType;
 
 typedef struct {
     TokenType type;
-    char *value;
+    char *lexeme;
     int line;
 } Token;
 
